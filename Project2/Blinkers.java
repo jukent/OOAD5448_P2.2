a51e99e305bc32940d74eeedde01ecb33f4f9f05
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-09-20 12:01:12 -0600

    Update Blinkers.java

--- Project2/Blinkers.java
+++ Project2/Blinkers.java
@@ -36,11 +36,11 @@ public class Blinkers extends Creatures {
         // Get a new hashtable to store possible rooms called 'possible_room_map'
         // Hashtable values point to a subset of the Room objects in the main dungeon map
         Hashtable<String, Room> possible_room_map = new Hashtable<String, Room>();
-        possible_room_map.putAll(dungeon.getMap());
+        possible_room_map.putAll(dungeon.getMap()); // Learned method from Geeks for Geeks: "How to Copy Map Content to Another Hashtable in Java?(https://www.geeksforgeeks.org/how-to-copy-map-content-to-another-hashtable-in-java/)
         possible_room_map.remove("0-1-1"); // Remove entrace room
         possible_room_map.remove(current_room.getName()); // Remove current room
 
-        // Randomly select one of the rooms
+        // Randomly select one of the rooms - learned from Stack Overflow question (https://stackoverflow.com/questions/38248381/pick-a-random-element-from-a-hashtable)
         // Convert values to an ArrayList of Room so that Rooms can grabbed via index
         ArrayList<Room> possible_rooms = new ArrayList<Room>(possible_room_map.values());
 
@@ -77,7 +77,7 @@ public class Blinkers extends Creatures {
             }
         }
                 
-        // Randomly select one of the rooms
+        // Randomly select one of the rooms - learned from Stack Overflow question (https://stackoverflow.com/questions/38248381/pick-a-random-element-from-a-hashtable)
         Random random = new Random();
         int random_index = random.nextInt(starting_rooms.size());
 
